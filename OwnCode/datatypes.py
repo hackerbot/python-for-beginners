46f29d36878a28d37a2745ae225642efafa2baab
hackerbot <ninh@ninh-macbookpro2.roam.corp.google.com>
2015-03-18 13:08:36 -0700

    Initial Commit
    Ch2 Up to elis statements

---
+++ OwnCode/datatypes.py
@@ -0,0 +1,24 @@
+a = 50
+b = 50.5
+c = int(b)
+d = round(50.51)
+e = float(a)
+g = '''\
+    This
+    Is\
+    A
+    String
+'''
+h = 'Ninh'
+f = 'Welcome, {}',format(h)
+
+print(type(a),a);
+print(type(b),b);
+print(type(c),c);
+print(type(d),d);
+print(type(e),e);
+print(type(f),f);
+print(type(g),g);
+
+x = (1,2,3,4)
+y= {'one':1,'two':2}
