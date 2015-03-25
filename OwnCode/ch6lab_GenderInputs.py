6acae8b2c519b9c3868676b412f0ff54b64d5918
hackerbot <ninh@ninh-macbookpro2.roam.corp.google.com>
2015-03-25 15:34:46 -0700

    Ch7 in Progress
    ch5lab Debugged, ch6 Debugged

--- OwnCode/ch6lab_GenderInputs.py
+++ OwnCode/ch6lab_GenderInputs.py
@@ -1,5 +1,10 @@
 data = input('Enter the first initial of your gender:\n')
-while data != 'm' or data != 'M' or data != 'f' or data != 'F':
+if data == 'm' or data == 'M':
+    print('You have entered "{}" that means you are a male!'.format(data))
+elif data == 'f' or data == 'F':
+    print('You have entered "{}" that means you are a female!'.format(data))
+else:
+    while data != 'm' or data != 'M' or data != 'f' or data != 'F':
         data = input('You have entered: "{}".\nPlease enter a valid initial of your gender:\n'.format(data))
         if data == 'm' or data == 'M':
             print('You have entered "{}" that means you are a male!'.format(data))
---
+++ OwnCode/ch7lab_APIKeyValidation.py
@@ -0,0 +1,10 @@
+string = '82914656273523:a4edFea2786DGex'
+
+sequence = string.split(':',2)
+print(string)
+
+for letter in string:
+    print(letter)
+    
+for number in string:
+    print(number)
