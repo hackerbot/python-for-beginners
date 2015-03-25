83d1545360218136cd10c02a6ceea2afdf98bbe6
hackerbot <ninh@ninh-macbookpro2.roam.corp.google.com>
2015-03-24 18:15:27 -0700

    CH6 Debug
    reordered loop to correct flow

--- OwnCode/ch6lab_GenderInputs.py
+++ OwnCode/ch6lab_GenderInputs.py
@@ -1,8 +1,9 @@
 data = input('Enter the first initial of your gender:\n')
-if data == 'm' or data == 'M':
-    print('You have entered "{}" that means you are a male!'.format(data))
-elif data == 'f' or data == 'F':
-    print('You have entered "{}" that means you are a female!'.format(data))
-else:
-    while data != 'm' or data != 'M' or data != 'f' or data != 'F':
+while data != 'm' or data != 'M' or data != 'f' or data != 'F':
         data = input('You have entered: "{}".\nPlease enter a valid initial of your gender:\n'.format(data))
+        if data == 'm' or data == 'M':
+            print('You have entered "{}" that means you are a male!'.format(data))
+            break
+        elif data == 'f' or data == 'F':
+            print('You have entered "{}" that means you are a female!'.format(data))
+            break
