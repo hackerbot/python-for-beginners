d0f37c8d3de9c6b9c0ac6763d1de0926b12d3818
hackerbot <ninh@ninh-macbookpro2.roam.corp.google.com>
2015-03-30 12:33:14 -0700

    ch8lab in progress

--- OwnCode/ch8lab_ReturnFunctionStringToDataType.py
+++ OwnCode/ch8lab_ReturnFunctionStringToDataType.py
@@ -1,12 +1,14 @@
 def datatyper():
     'Takes input and tells you what it is also returns same type of output.'
-    boolean = True or False
+    boolean = 'false' or 'true' or False or True
     integer = int(1)
-    data = input("Enter a string that you want to classify it's data type:\n")
-    if type(data) == type(boolean):
+    print("Enter a string that you want to classify it's data type:")
+    data = input()
+    type(data) == type(input())
+    if type(input) == type(boolean):
         return boolean(data)
         print('You have entered "{}" and this is a boolean.'.format(data))
-    elif type(data) == type(integer):
+    elif type(input) == type(integer):
         print('You have entered "{}" and this is an integer.'.format(data))
         return integer(data)
     else:
@@ -15,14 +17,15 @@ def datatyper():
 
 datatyper()
 
-boolean = True or False
-integer = int(1)
+#boolean = True or False
+#integer = int(1)
 #data = int(boolean(input("Enter a string that you want to classify it's data type:\n")))
-if type(data) == type(boolean):
-    print('You have entered "{}" and this is a boolean.'.format(data))
-elif type(data) == type(integer):
-    print('You have entered "{}" and this is an integer.'.format(data))
-else:
-    print('You have entered "{}" and this is an alphanumeric.'.format(data))
-    
-print(type(data))
+#if data = int(input("Enter a string that you want to classify it's data type:\n")):
+#    return print(type(data))
+#if type(data) == type(boolean):
+#    print('You have entered "{}" and this is a boolean.'.format(data))
+#elif type(data) == type(integer):
+#    print('You have entered "{}" and this is an integer.'.format(data))
+#else:
+#    print('You have entered "{}" and this is an alphanumeric.'.format(data))    
+#print(type(data))
