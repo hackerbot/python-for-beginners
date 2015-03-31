148d8dd1b1632be23609171fdd85473ae5f58043
hackerbot <ninh@ninh-macbookpro2.roam.corp.google.com>
2015-03-31 09:12:28 -0700

    ch8lab finished
    ch8lab redone and debugged

--- OwnCode/ch8lab_ReturnFunctionStringToDataType.py
+++ OwnCode/ch8lab_ReturnFunctionStringToDataType.py
@@ -1,31 +1,33 @@
 def datatyper():
-    'Takes input and tells you what it is also returns same type of output.'
-    boolean = 'false' or 'true' or False or True
-    integer = int(1)
-    print("Enter a string that you want to classify it's data type:")
-    data = input()
-    type(data) == type(input())
-    if type(input) == type(boolean):
-        return boolean(data)
+    'Takes input and returns same type of input as an output.'
+    boolean = ('false' or 'true' or 'False' or 'True')
+    integer = (1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 0)
+    data = input("Enter data into function that returns similar data:\n")
+    if data == boolean:
+        return boolean
         print('You have entered "{}" and this is a boolean.'.format(data))
-    elif type(input) == type(integer):
-        print('You have entered "{}" and this is an integer.'.format(data))
-        return integer(data)
-    else:
-        print('You have entered "{}" and this is an alphanumeric.'.format(data))
+    elif data == integer:
+        return integer
+        print('You have entered "{}" and this is a digit.'.format(data))
+    elif data != (boolean or integer):
         return data
+        print('You have entered "{}" and this is an alphanumeric.'.format(data))
+    else:
+        return 'Input Error'
+print(datatyper())
 
-datatyper()
-
-#boolean = True or False
-#integer = int(1)
-#data = int(boolean(input("Enter a string that you want to classify it's data type:\n")))
-#if data = int(input("Enter a string that you want to classify it's data type:\n")):
-#    return print(type(data))
-#if type(data) == type(boolean):
-#    print('You have entered "{}" and this is a boolean.'.format(data))
-#elif type(data) == type(integer):
-#    print('You have entered "{}" and this is an integer.'.format(data))
-#else:
-#    print('You have entered "{}" and this is an alphanumeric.'.format(data))    
-#print(type(data))
+#def datatyper():
+#    'Takes input and tells you what it is also returns same type of output.'
+#    boolean = 'false' or 'true' or False or True
+#    integer = int(1)
+#    print("Enter a string that you want to classify it's data type:")
+#    data = input()
+#    if type(input) == type(boolean):
+#        return 'You have entered "{}" and this is a boolean.'.format(data)
+#    elif type(input) == type(integer):
+#        return 'You have entered "{}" and this is an integer.'.format(data)
+#    elif type(input) != (type(boolean) or type(integer)):
+#        return 'You have entered "{}" and this is an alphanumeric.'.format(data)
+#    else:
+#        return 'Input Error'
+#print(datatyper())
