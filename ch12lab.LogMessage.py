c506a1b0f16e4675669cdbdd7c21b89867bb9bea
hackerbot <ninh@ninh-macbookpro2.roam.corp.google.com>
2015-05-05 15:46:33 -0700

    rename

---
+++ ch12lab.LogMessage.py
@@ -0,0 +1,34 @@
+def LogFile():
+    from ch10lab.LogMessageFile import LogMessageFile
+    print(LogMessageFile)
+
+def LogDb():
+    from ch11lab.LogMessageDbSQL import LogMessageDbSQL
+    print(LogMessageDbSQL)
+    
+def select():
+    selection = int(input('Would you like to read and edit a file or database?\nPress 1 to run LogFile()\nPress 2 to run LogDb()\nMake your selection:\n'))
+    while selection != 1 or 2:
+        selection = int(input('Please enter your selection again:\n'))
+        if selection == 1:
+            LogFile()
+            selection2 = int(input('Would you like to edit database?\nPress 1 = Yes\nPress 2 = No\n'))
+            if selection2 == 1:
+                LogDb()
+                print('Congratulations you have ran both classes and you are good to go! :)')
+                break
+            elif selection2 == 2:
+                print('You are good to go! :)')
+                break
+        elif selection == 2:
+            LogDb()
+            selection2 = int(input('Would you like to edit database?\nPress 1 = Yes\nPress 2 = No\n'))
+            if selection2 == 1:
+                LogFile()
+                print('Congratulations you have ran both classes and you are good to go! :)')
+                break
+            elif selection2 == 2:
+                print('You are good to go! :)')
+                break
+                
+select()
